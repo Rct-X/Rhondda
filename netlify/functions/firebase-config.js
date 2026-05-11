87b3d57893f32ad9f179e893dc2ea70cfc27d715
Rct-X <valleysameday@gmail.com>
2026-05-11 20:37:18 +0100

    Add Firebase configuration handler

---
+++ netlify/functions/firebase-config.js
@@ -0,0 +1,29 @@
+exports.handler = async () => {
+
+  return {
+    statusCode: 200,
+
+    headers: {
+      "Content-Type": "application/json"
+    },
+
+    body: JSON.stringify({
+
+      apiKey: process.env.RN_FIREBASE_API_KEY,
+
+      authDomain: process.env.RN_FIREBASE_AUTH_DOMAIN,
+
+      projectId: process.env.RN_FIREBASE_PROJECT_ID,
+
+      storageBucket: process.env.RN_FIREBASE_STORAGE_BUCKET,
+
+      messagingSenderId:
+        process.env.RN_FIREBASE_MESSAGING_SENDER_ID,
+
+      appId: process.env.RN_FIREBASE_APP_ID
+
+    })
+
+  };
+
+};
