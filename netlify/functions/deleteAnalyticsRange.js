542b90f3d76227b39e09a75b73dfac6d698f31f1
Rct-X <valleysameday@gmail.com>
2026-06-04 22:45:50 +0100

    Update Firebase credentials to use RN environment variables

--- netlify/functions/deleteAnalyticsRange.js
+++ netlify/functions/deleteAnalyticsRange.js
@@ -4,9 +4,9 @@ if (!admin.apps.length) {
 
   admin.initializeApp({
     credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+      projectId: process.env.RN_FIREBASE_PROJECT_ID,
+      clientEmail: process.env.RN_FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.RN_FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
     })
   });
 }
