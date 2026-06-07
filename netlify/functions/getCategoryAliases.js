8c72c4c18f800ab7bcb2a7177baf63f95a5bc2d6
Rct-X <valleysameday@gmail.com>
2026-06-08 00:50:40 +0100

    Create getCategoryAliases.js

---
+++ netlify/functions/getCategoryAliases.js
@@ -0,0 +1,8 @@
+const categoryAliases = require("../../shared/categoryAliases");
+
+exports.handler = async () => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(categoryAliases)
+  };
+};
