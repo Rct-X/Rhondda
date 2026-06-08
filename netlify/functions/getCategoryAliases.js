d4a0481319592c079f4364033c6f3ec30425e118
Rct-X <valleysameday@gmail.com>
2026-06-08 02:10:22 +0100

    Add JSON response headers to getCategoryAliases

--- netlify/functions/getCategoryAliases.js
+++ netlify/functions/getCategoryAliases.js
@@ -1,8 +1,13 @@
 const categoryAliases = require("./shared/categoryAliases");
 
 exports.handler = async () => {
+
   return {
     statusCode: 200,
+    headers: {
+      "Content-Type": "application/json"
+    },
     body: JSON.stringify(categoryAliases)
   };
+
 };
