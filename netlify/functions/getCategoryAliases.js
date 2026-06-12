f163b5a6ae0cc23ec009bfffe0bf0eacbbfe5bba
Rct-X <valleysameday@gmail.com>
2026-06-12 20:57:04 +0100

    Update getCategoryAliases.js

--- netlify/functions/getCategoryAliases.js
+++ netlify/functions/getCategoryAliases.js
@@ -1,13 +1,30 @@
 const categoryAliases = require("./categoryAliases");
 
+function slugify(str = "") {
+  return str
+    .toLowerCase()
+    .trim()
+    .replace(/&/g, "and")
+    .replace(/[^a-z0-9]+/g, "-")
+    .replace(/^-+|-+$/g, "");
+}
+
 exports.handler = async () => {
+
+  const categories = Object.keys(categoryAliases).map(name => ({
+    name,
+    slug: slugify(name)
+  }));
 
   return {
     statusCode: 200,
     headers: {
       "Content-Type": "application/json"
     },
-    body: JSON.stringify(categoryAliases)
+    body: JSON.stringify({
+      ok: true,
+      categories
+    })
   };
 
 };
