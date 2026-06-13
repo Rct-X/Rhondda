a8285e606084b95d5606a1b1316ec9c441ebd923
Rct-X <valleysameday@gmail.com>
2026-06-13 13:26:03 +0100

    Update send-manual-email.js

--- netlify/functions/send-manual-email.js
+++ netlify/functions/send-manual-email.js
@@ -1,17 +1,28 @@
-fetch("/.netlify/functions/sendSeededListingEmail", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify({
-    name: "Purple Shoots Shop",
-    email: "shop@purpleshoots.co.uk",
-    businessName: "Purple Shoots Shop",
-    slug: "purple-shoots-shop",
-    townSlug: "pontypridd",
-    categorySlug: "shops"
-  })
-})
-.then(r => r.json())
-.then(console.log)
-.catch(console.error);
+exports.handler = async () => {
+
+  const response = await fetch(
+    "https://rctx.co.uk/.netlify/functions/sendSeededListingEmail",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        name: "Purple Shoots Shop",
+        email: "shop@purpleshoots.co.uk",
+        businessName: "Purple Shoots Shop",
+        slug: "purple-shoots-shop",
+        townSlug: "pontypridd",
+        categorySlug: "shops"
+      })
+    }
+  );
+
+  const data = await response.json();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data)
+  };
+
+};
