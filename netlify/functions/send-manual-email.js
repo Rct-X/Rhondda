3fd2ddfc6b372133f1d80298da8d5461a79f9e5e
Rct-X <valleysameday@gmail.com>
2026-06-13 12:31:43 +0100

    Implement email sending for failed addresses
    Add functionality to send emails to a list of failed addresses.

--- netlify/functions/send-manual-email.js
+++ netlify/functions/send-manual-email.js
@@ -1,1 +1,21 @@
 ///manual EMAILS////
+const failed = [
+  "stephenprice.plumber@googlemail.com",
+  "shop@purpleshoots.co.uk",
+  "fairytalesbabywear@hotmail.com",
+  "gemsfades@gmail.com",
+  "enquiries@freindsofanimalswales.org.uk",
+  "Support@rctx.co.uk"
+];
+
+failed.forEach(email => {
+  fetch("/.netlify/functions/send-claim-email", {
+    method: "POST",
+    body: JSON.stringify({
+      name: "Business Owner",
+      email,
+      businessName: "Your Business",
+      slug: "your-slug"
+    })
+  });
+});
