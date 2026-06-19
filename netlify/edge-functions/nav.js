3b656796bc6339c00583f401d0ce02de1163f9b6
Rct-X <valleysameday@gmail.com>
2026-06-19 22:50:06 +0100

    Add nav.js for rendering navigation HTML

---
+++ netlify/edge-functions/nav.js
@@ -0,0 +1,13 @@
+return new Response(`
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="/css/nav.css">
+</head>
+<body>
+  ${nav}
+  ${html}
+  <script src="/js/footer-nav-faq.js"></script>
+</body>
+</html>
+`);
